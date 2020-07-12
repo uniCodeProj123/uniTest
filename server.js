5f99dfc20edaeba5c89aa383dc72775c4f8f2bc5
Hussain <anas.hussain@capgemini.com>
2020-07-12 19:15:48 +0100

    intial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine','ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to mongoose'))
+
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
+
+
+
+// app.get(
+//   '/',
+//   function(req,res){
+//     res.send('Hello, World!');
+//   }
+// );
+//
+// var port = 3000;
+// app.listen(port,function(){
+//   console.log('Listening on port'+ port);
+// });
